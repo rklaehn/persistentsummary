69b8592b0d385d1f5f50b1d49263969376d5ca48
Rüdiger Klaehn <rklaehn@gmail.com>
2016-01-29 15:17:34 +0100

    Added some scaladoc text

--- src/main/scala/com/rklaehn/persistentsummary/PersistentSummary.scala
+++ src/main/scala/com/rklaehn/persistentsummary/PersistentSummary.scala
@@ -2,6 +2,13 @@ package com.rklaehn.persistentsummary
 
 import scala.collection.immutable._
 
+/**
+  * Creates persistent summaries for various collection types. The return value of these methods must be stored
+  * somewhere for the summaries calculated by it to be persistent. The underlying implementation uses a guava cache with
+  * weak keys.
+  *
+  * https://code.google.com/p/guava-libraries/wiki/CachesExplained
+  */
 object PersistentSummary {
 
   def treeSet[K, S](summary: Summary[K, S]): (TreeSet[K] => S) = PersistentSummaryHelper.treeSet(summary)
--- src/main/scala/com/rklaehn/persistentsummary/Summary.scala
+++ src/main/scala/com/rklaehn/persistentsummary/Summary.scala
@@ -1,12 +1,42 @@
 package com.rklaehn.persistentsummary
 
+/**
+  * Typeclass for summaries.
+  *
+  * The empty element and the combine operation must form a monoid.
+  *
+  * @tparam A the element type
+  * @tparam S the summary type
+  */
 trait Summary[A, S] {
 
+  /**
+    * the empty summary
+    */
   def empty: S
 
+  /**
+    * create a summary for a single element
+    * @param value the element to summarize
+    * @return the summary
+    */
   def apply(value: A): S
 
+  /**
+    * combine two summary values. This method must be associative
+    * @param a a summary
+    * @param b a summary
+    * @return the combined summary
+    */
   def combine(a: S, b: S): S
 
+  /**
+    * convenience method to combine three summaries.
+    * This must be equivalent to combine(combine(a, b), c)
+    * @param a a summary
+    * @param b a summary
+    * @param c a summary
+    * @return the combined summary
+    */
   def combine3(a: S, b: S, c: S): S = combine(combine(a, b), c)
 }
